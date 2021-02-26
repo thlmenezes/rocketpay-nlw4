56a1a91a1737832f16bdb18e62e516a55b744ff0
Thales Menezes <thlmenezes@pm.me>
2021-02-26 10:34:59 -0300

    feat(router): add basic auth to routes

--- config/config.exs
+++ config/config.exs
@@ -22,6 +22,10 @@ config :rocketpay, Rocketpay.Repo,
   migration_primary_key: [type: :binary_id],
   migration_foreign_key: [type: :binary_id]
 
+config :rocketpay, :basic_auth,
+  username: System.get_env("API_USERNAME"),
+  password: System.get_env("API_PASSWORD")
+
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
--- lib/rocketpay_web/router.ex
+++ lib/rocketpay_web/router.ex
@@ -1,14 +1,25 @@
 defmodule RocketpayWeb.Router do
   use RocketpayWeb, :router
 
+  import Plug.BasicAuth
+
   pipeline :api do
     plug :accepts, ["json"]
+  end
+
+  pipeline :auth do
+    plug :basic_auth, Application.get_env(:rocketpay, :basic_auth)
+    # Application.compile_env
   end
 
   scope "/api", RocketpayWeb do
     pipe_through :api
 
     post "/users", UsersController, :create
+  end
+
+  scope "/api", RocketpayWeb do
+    pipe_through [:api, :auth]
 
     post "/accounts/:id/deposit", AccountsController, :deposit
     post "/accounts/:id/withdraw", AccountsController, :withdraw
